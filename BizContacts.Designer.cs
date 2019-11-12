ec11ef169932890dd3f7d6641ecf42bd0103a8be
Askar Karasaev <askar.karasaev@gmail.com>
2019-11-12 11:33:07 +0900

    Create the main form and business forms

--- BizContacts.Designer.cs
+++ BizContacts.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class BizContacts
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // BizContacts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "BizContacts";
+            this.Text = "BizContacts";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
